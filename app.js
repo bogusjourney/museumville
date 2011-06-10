980ccbd742aebad480550d8cc94f754078da3029
Niklas Lindström <lindstream@gmail.com>
2011-06-10 15:34:47 +0200

    Working EJS views

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -30,7 +30,8 @@ app.configure('production', function(){
 
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'Express',
+    items: ['Item A', 'Item B']
   });
 });
 
