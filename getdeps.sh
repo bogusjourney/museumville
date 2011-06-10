5ac9a56f572848a333f6a21fc6cc0e39c48d1b50
Niklas Lindström <lindstream@gmail.com>
2011-06-10 20:58:06 +0200

    More JS goodies for profit and fun

---
+++ getdeps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname $0)"
+force=("$1" == "-f")
+
+download() {
+  if [ $force ] || [[ ! -e $2 ]]; then
+    echo "Downloading <$1> to: $2"
+    curl -# $1 -o $2
+  fi
+}
+
+download http://code.jquery.com/jquery-1.6.1.min.js public/javascripts/jquery.min.js
+download http://documentcloud.github.com/underscore/underscore-min.js public/javascripts/underscore-min.js
+
